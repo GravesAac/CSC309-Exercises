30dfeee48ed6ae0d7cafd72daa39c919ddd25e63
xiaguan2 <markus@markus.com>
2024-09-14 23:07:48 -0400

    Update files

---
+++ E2/task2.js
@@ -0,0 +1,54 @@
+
+const isEmpty = str => !str.trim().length;
+
+function addListElement() {
+    let buttonclick = document.getElementById("addItemButton");
+    buttonclick.addEventListener("click", function () {
+        let inputField = document.getElementById("itemInput");
+        if (!isEmpty(inputField.value)) {
+            let li = document.createElement('li');
+            li.textContent = inputField.value;
+            document.getElementById("itemList").appendChild(li);
+            inputField.value = ''; // clear the input field after each click;
+        }
+    });
+}
+
+function validateForm() {
+    let buttonclick = document.getElementById("submitButton");
+    buttonclick.addEventListener("click", function () {
+        let fields = ["name", "email", "password", "repeatPassword"];
+        let fieldLength = fields.length;
+        let fieldName = "";
+        for (var i = 0; i < fieldLength; i++) {
+            fieldName = fields[i];
+            if (document.getElementById("registrationForm")[fieldName].value === "") {
+                alert(fieldName + " cannot be empty, please re-enter your information");
+                document.getElementById("registrationForm")[fieldName].style.borderColor = "red";
+                return false;
+            }
+        }
+        let success = document.getElementById("successMessage");
+        success.innerHTML = "wow, nice submission!!!!!!!!";
+        return true;
+    });
+}
+
+function todoApp() {
+    let buttonClick = document.getElementById("addTodoButton");
+    buttonClick.addEventListener("click", function () {
+        let inputField = document.getElementById("newTodo");
+        if (inputField.value !== "") {
+            let li = document.createElement('li'); // li should be created for each to do in this case
+            // create the checkbox
+            let checkbox = document.createElement("INPUT");
+            checkbox.setAttribute("type", "checkbox");
+            li.appendChild(checkbox);
+            let textNode = document.createTextNode(inputField.value);
+            li.appendChild(textNode);
+            document.getElementById("todoList").appendChild(li);
+            inputField.value = "";
+        }
+    });
+}
+
