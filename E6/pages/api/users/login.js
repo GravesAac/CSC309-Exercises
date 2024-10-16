1a87042ea507a6f341e00490de83c0d72da84c79
xiaguan2 <markus@markus.com>
2024-10-15 22:57:38 -0400

    Update files

--- E6/pages/api/users/login.js
+++ E6/pages/api/users/login.js
@@ -1,4 +1,4 @@
-import prisma from '@/utils/db';
+import prisma from '@/lib/prisma';
 import { comparePassword, generateAccessToken, generateRefreshToken } from '@/utils/auth';
 
 export default async function handler(req, res) {
@@ -8,27 +8,22 @@ export default async function handler(req, res) {
 
   const { username, password } = req.body;
 
-  // Validate input
   if (!username || !password) {
     return res.status(400).json({ error: 'Username and password are required.' });
   }
 
   try {
-    // Find user by username
     const user = await prisma.user.findUnique({
       where: { username },
     });
 
-    // Check if user exists and the password is valid
     if (!user || !(await comparePassword(password, user.password))) {
       return res.status(401).json({ error: 'Invalid username or password.' });
     }
 
-    // Create payload for the JWT
     const payload = {
       username: user.username,
       role: user.role,
-      expiresAt: new Date(Date.now() + 15 * 60 * 1000), 
     };
 
     const accessToken = generateAccessToken(payload);
