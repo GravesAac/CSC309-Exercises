352046ee044afa7c40d2c69da10a1191f2beb7e4
xiaguan2 <markus@markus.com>
2024-10-15 22:57:15 -0400

    Update files

--- E6/pages/api/protected.js
+++ E6/pages/api/protected.js
@@ -1,7 +1,6 @@
 import { withAuth } from '@/utils/middleware';
 
 async function protectedHandler(req, res) {
-  // This will only run if the token is valid
   res.status(200).json({ message: 'Access granted to protected route', user: req.user });
 }
 
