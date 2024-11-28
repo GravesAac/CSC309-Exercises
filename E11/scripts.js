c728d1c013bea0224f51cc7b193ee5ffc0d53bef
xiaguan2 <markus@markus.com>
2024-11-28 14:22:45 -0500

    Update files

---
+++ E11/scripts.js
@@ -0,0 +1,12 @@
+function getBaseUrl() {
+    return "https://o0oaomql7c.execute-api.us-east-2.amazonaws.com/";
+  }
+  
+  function getUTorId() {
+    return "xiaguan2";
+  }
+  
+  module.exports = {
+    getBaseUrl,
+    getUTorId,
+  };
